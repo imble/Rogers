73d07c6d6529d1b68b0b5eb5ca01f0c6bdba0972
imble <149072007+imble@users.noreply.github.com>
2023-10-26 05:37:58 -0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_8t065rq", "template_uz537o2", formData)
+    emailjs.send("service_8t065rq", "template_uz537o, formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
